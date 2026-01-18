fe880fac020ce03433676fd1c2054714e824d6e1
cpud <pudnec14@outlook.com>
2026-01-18 16:06:07 -0500

    movement and containers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,7 +10,11 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    new_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
     
     # game loop
     while True:
@@ -20,8 +25,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        #new_player.update(dt)
+        updatable.update(dt)    
         screen.fill("black")
+        #new_player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+        
+        # limit 60fps
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
